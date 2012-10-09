a13d4b8b200d14ac90c97cd812a8a681e7d82ab5
Jens Petersen <petersen@redhat.com>
2012-10-09 13:22:50 +0900

    test-rebuild.sh: use "fedpkg verrel" to find built rpms

--- test-rebuild.sh
+++ test-rebuild.sh
@@ -8,18 +8,18 @@ PKG=$1
 
 [ -d "$PKG" ] && cd $PKG
 
+ARCH=$(arch)
+
+#if [ -d $ARCH ]; then
+#  echo Please move existing $ARCH/
+#  exit 1
+#fi
+
 if [ -n "$2" ]; then
   fedpkg switch-branch $2
 fi
 
 sudo yum-builddep $PKG.spec
-
-ARCH=$(arch)
-
-if [ -d $ARCH ]; then
-  echo Please move existing $ARCH/
-  exit 1
-fi
 
 fedpkg local
 
@@ -29,7 +29,9 @@ mkdir -p $TMP/
 
 cd $ARCH
 
-PKGS=$(rpm -qp *)
+VERREL=$(fedpkg verrel | sed -e "s/^$PKG-//")
+
+PKGS=$(rpm -qp *-$VERREL)
 
 for i in $PKGS; do
   sudo yum install $i
