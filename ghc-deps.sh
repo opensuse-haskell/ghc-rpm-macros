69bd8d487fc4955cdf9b0271307d32ca231ab851
Ondřej Súkup <mimi.vx@gmail.com>
2018-04-04 22:49:22 +0200

    Exclude internal libraries from dependecy generator
    It reads all conf files in package.conf.d and for all packages with name
    pattern z-*-z-* stores id in array, then compares this id's with id
    returned from ghc-pkg call

--- ghc-deps.sh
+++ ghc-deps.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # find rpm provides and requires for Haskell GHC libraries
 
-[ $# -ne 2 ] && echo "Usage: `basename $0` [--provides|--requires] %{buildroot}%{ghclibdir}" && exit 1
+[ $# -ne 2 ] && echo "Usage: $(basename $0) [--provides|--requires] %{buildroot}%{ghclibdir}" && exit 1
 
 set +x
 
@@ -12,35 +12,47 @@ pkgconfdir=$pkgbasedir/package.conf.d
 ghc_pkg="/usr/lib/rpm/ghc-pkg-wrapper $pkgbasedir"
 
 case $mode in
-    --provides) field=id ;;
-    --requires) field=depends ;;
-    *) echo "`basename $0`: Need --provides or --requires" ; exit 1
+--provides) field=id ;;
+--requires) field=depends ;;
+*)
+    echo "$(basename $0): Need --provides or --requires"
+    exit 1
+    ;;
 esac
 
 ghc_ver=$(basename $pkgbasedir | sed -e s/ghc-//)
 
 files=$(cat)
-
 #cabal_ver=$(ghc-pkg --global --simple-output list Cabal | sed -e "s/Cabal-//")
+
+declare -a exclude_dep
+all_files=$(ls $pkgconfdir/*)
+for x in $all_files; do
+    if grep -q 'name: z-.*-z-.*' $x; then
+        exclude_dep+=($(sed -rn '/id: /s/id: (.*)/\1/ p' <$x))
+    fi
+done
 
 for i in $files; do
     case $i in
-        # exclude builtin_rts.conf
-	$pkgconfdir/*-*.conf)
-            case $ghc_ver in
-                8.*) id=$(grep "id: " $i | sed -e "s/id: //") ;;
-                *) id=$(echo $i | sed -e "s%$pkgconfdir/%%" -e "s%.conf%%") ;;
-            esac
-	    ids=$($ghc_pkg field $id $field | sed -e "s/rts//" -e "s/bin-package-db-[^ ]\+//")
+    # exclude builtin_rts.conf
+    $pkgconfdir/*-*.conf)
+        case $ghc_ver in
+        8.*) id=$(grep "id: " $i | sed -e "s/id: //") ;;
+        *) id=$(echo $i | sed -e "s%$pkgconfdir/%%" -e "s%.conf%%") ;;
+        esac
 
-	    for d in $ids; do
-		case $d in
-		    *-*) echo "ghc-devel($d)" ;;
-		    *) ;;
-		esac
-	    done
-	    ;;
-        *)
-            ;;
+        ids=$($ghc_pkg field $id $field | sed -e "s/rts//" -e "s/bin-package-db-[^ ]\+//")
+
+        for d in $ids; do
+            if [[ ! " ${exclude_dep[@]} " =~ " ${d} " ]]; then
+                case $d in
+                *-*) echo "ghc-devel($d)" ;;
+                *) ;;
+                esac
+            fi
+        done
+        ;;
+    *) ;;
     esac
 done
